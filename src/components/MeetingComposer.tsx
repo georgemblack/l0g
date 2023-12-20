6a5c3fe288988d383bfff94acecbc8c104240561
George Black <contact@george.black>
2023-12-20 14:34:58 +0000

    Add basic tagging to meetings

--- src/components/MeetingComposer.tsx
+++ src/components/MeetingComposer.tsx
@@ -2,8 +2,20 @@ import { useState } from "react";
 
 import { db } from "../data/DB";
 
+const availableTags = [
+  "Team",
+  "Standup",
+  "One-One",
+  "Rodo",
+  "Janice",
+  "Francis",
+  "Avery",
+  "Emmett",
+];
+
 function MeetingComposer() {
   const [description, setDescription] = useState<string>("");
+  const [tags, setTags] = useState<string[]>([]);
 
   const handleSubmit = () => {
     if (description === "") return;
@@ -25,7 +37,19 @@ function MeetingComposer() {
         onChange={(event) => setDescription(event.target.value)}
         autoFocus={true}
       ></textarea>
-      <div className="flex justify-end">
+      <div className="flex justify-between">
+        <div className="flex flex-wrap gap-2 mt-2">
+          {availableTags.map((tag) => (
+            <span
+              style={{ backgroundColor: "rgba(255,255,255,0.15)" }}
+              className="text-xs text-white py-1 px-2 rounded cursor-pointer"
+              onClick={() => setTags([...tags, tag])}
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+
         <button
           type="submit"
           className="text-sm text-white opacity-50 px-2"
